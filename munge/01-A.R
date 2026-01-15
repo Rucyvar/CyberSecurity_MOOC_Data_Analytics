bc8e9f5c78139164f8bcd1a932741b57b2d310e0
Rucyvar <vasoyarushi@gmail.com>
2026-01-15 16:46:00 +0000

    Completed data munging for CRISP-DM Cycle 1
    Implemented data munging (pre-processing) for CRISP-DM Cycle 1
    
    Munging focuses on enrolment and step activity datasets
    
    Pre-processing logic added under the munge/ directory following ProjectTemplate conventions
    
    Data cleaning, filtering, and transformation performed to support learner engagement analysis
    
    No manual data loading used in report or analysis files
    
    Raw data remains excluded from version control via .gitignore
    
    Munged data prepared for downstream Data Analysis and Evaluation stages
    
    Cycle 1 is now ready for exploratory analysis and interpretation

--- munge/01-A.R
+++ munge/01-A.R
@@ -1,1 +1,53 @@
-# Example preprocessing script.
+# ------------------------------------------------------------
+# Cycle 1 Pre-processing: Enrolment & Step Activity Data
+# ------------------------------------------------------------
+
+# Identify datasets loaded by ProjectTemplate
+enrolment_names <- ls(pattern = "enrolments$")
+step_names      <- ls(pattern = "step\\.activity$")
+
+# Combine enrollments
+enrolments_all <- purrr::map_dfr(
+  enrolment_names,
+  function(df_name) {
+    get(df_name) %>%
+      dplyr::mutate(run_id = df_name)
+  }
+)
+
+# Combine step activity
+step_activity_all <- purrr::map_dfr(
+  step_names,
+  function(df_name) {
+    get(df_name) %>%
+      dplyr::mutate(run_id = df_name)
+  }
+)
+
+# Data Cleaning(Remove rows where all values are missing)
+enrolments_all_cleaned <- enrolments_all %>%
+  dplyr::filter(!dplyr::if_all(dplyr::everything(),is.na))
+step_activity_all_cleaned <- step_activity_all %>%
+  dplyr::filter(!dplyr::if_all(dplyr::everything(),is.na))
+
+#Summaries enrollment and step activity by course run
+
+enrolment_summary <- enrolments_all_cleaned %>%
+  dplyr::group_by(run_id) %>%
+  dplyr::summarise(
+    total_step_activity = dplyr::n(),
+    .groups = 'drop'
+  )
+step_activity_summary <- step_activity_all_cleaned %>%
+  dplyr::group_by(run_id) %>%
+  dplyr::summarise(
+    total_step_activity = dplyr::n(),
+    .groups = 'drop'
+  )
+
+engagement_summary <- enrolment_summary %>%
+  dplyr::left_join(step_activity_summary, by='run_id')
+
+cache('enrolments_all_cleaned')
+cache('step_activity_all_cleaned')
+cache('engagement_summary')
